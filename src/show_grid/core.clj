445c47aa08f63fc1f7e650ddecc278f3101c3c07
Adrian Mowat <adrian.mowat@gmail.com>
2011-10-26 08:51:40 +0100

    Initial version

---
+++ src/show_grid/core.clj
@@ -0,0 +1,26 @@
+(ns show-grid.core)
+(import '(javax.swing JFrame JLabel JPanel JTable JEditorPane JScrollPane))
+(import '(javax.swing.table DefaultTableModel))
+
+(defn row? [data] 
+	(or (seq? data) (vector? data) (map? data)))
+
+(defn tag [data]
+	(if (row? data) 
+		(str "<tr>" (apply str (map #(tag %) data)) "</tr>")
+		(str "<td>" data "</td>")))
+
+(defn to-html [data] 
+	(let [result (str "<table border='1' width='100%'>" (tag data) "</table>")]
+		result))
+			
+(defn show-grid [data] 
+	(let [
+		editor-pane (JEditorPane. "text/html" (to-html data))
+		scroll-pane (JScrollPane. editor-pane)
+		frame (JFrame. "show-grid") ]
+			(doto frame
+				(.setSize 250 400)
+				(.setContentPane scroll-pane)
+				(.setVisible true)))
+				nil)
---
+++ test/show_grid/test/core.clj
@@ -0,0 +1,6 @@
+(ns show-grid.test.core
+  (:use [show-grid.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
